384e37ace714072c9568575d6916b63118f9ac6a
sarang <saranggp2018@gmail.com>
2025-02-27 21:43:37 +0530

    did 2 part

--- Work/Data/hello.py
+++ Work/Data/hello.py
@@ -1,15 +1,26 @@
 print("hello world")
 
-bill_thickness = 0.11 * 0.001 # Meters (0.11 mm)
-sears_height = 442 # Height (meters)
-num_bills = 1
-day = 1
+# bill_thickness = 0.11 * 0.001 # Meters (0.11 mm)
+# sears_height = 442 # Height (meters)
+# num_bills = 1
+# day = 1
 
-while num_bills * bill_thickness < sears_height:
-    print(day, num_bills, num_bills * bill_thickness)
-    day = day + 1
-    num_bills = num_bills * 2
+# while num_bills * bill_thickness < sears_height:
+#     print(day, num_bills, num_bills * bill_thickness)
+#     day = day + 1
+#     num_bills = num_bills * 2
 
-print('Number of days', day)
-print('Number of bills', num_bills)
-print('Final height', num_bills * bill_thickness)
+# print('Number of days', day)
+# print('Number of bills', num_bills)
+# print('Final height', num_bills * bill_thickness)
+
+# height = 100
+
+# for i in range(10):
+#     height  = height * (3 / 5 )
+#     print(height)
+
+c = 4 + True
+d = 5 - False
+print(c)
+print(d)
