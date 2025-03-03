a1e4b20d96c0af59c42bfde5e0ffbfd5a3bb5eb1
sarang <saranggp2018@gmail.com>
2025-03-03 16:44:30 +0530

    learn scripting

--- Work/report.py
+++ Work/report.py
@@ -1,4 +1,5 @@
 import csv;
+from collections import defaultdict
 
 portfolio = [
     ('GOOG', 100, 490.1),
@@ -8,3 +9,43 @@ portfolio = [
     ('GOOG', 75, 572.45),
     ('AA', 50, 23.15)
 ]
+
+holdings = defaultdict(list)
+for name, shares, price in portfolio:
+    holdings[name].append((shares, price))
+print(holdings['IBM'])
+
+
+import csv
+f = open('Data/portfolio.csv')
+rows = csv.reader(f)
+headers = next(rows)
+headers
+
+def read_prices(filename):
+    prices = {}
+    with open(filename) as f:
+        f_csv = csv.reader(f)
+        for row in f_csv:
+            prices[row[0]] = float(row[1])
+    return prices
+
+def read_portfolio(filename):
+    '''
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    '''
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            record = dict(zip(headers, row))
+            stock = {
+                'name' : record['name'],
+                'shares' : int(record['shares']),
+                'price' : float(record['price'])
+            }
+            portfolio.append(stock)
+    return portfolio
